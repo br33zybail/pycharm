534edb467b45ce9e73d46b4b46293dd63fe1fbce
Jackson <jack@sigmaactuary.com>
2025-08-04 13:35:50 -0500

    logit probs

--- logit_probs.py
+++ logit_probs.py
@@ -3,3 +3,8 @@ logits = [2.0, 1.0, 0.1]
 exp_logits = np.exp(logits)
 probs = exp_logits / np.sum(exp_logits)
 print (probs)
+
+logits2 = [6.0, 2.0, 1.0, 0.05]
+exp_logits2 = np.exp(logits2)
+probs2 = exp_logits2 / np.sum(exp_logits2)
+print(probs2)
